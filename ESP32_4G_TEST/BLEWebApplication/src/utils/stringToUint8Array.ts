06987dcf01b80d28528cec4b5d5f0be444e0900e
Vinay Bhupathi <vinay.bhupati@bridgethings.com>
2026-04-14 20:10:38 +0530

    Revert "Updated BLE web app"
    This reverts commit 0b73cdbb8cf6820ca9a4ffe42905d20bd9e940df.

--- ESP32_4G_TEST/BLEWebApplication/src/utils/stringToUint8Array.ts
+++
@@ -1,6 +0,0 @@
-const stringToUint8Array = (str: string) => {
-  const encoder = new TextEncoder();
-  return encoder.encode(str);
-};
-
-export default stringToUint8Array;
--- ESP32_4G_TEST/app_src/src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-declare const __APP_VERSION__: string;
-declare const __APP_GIT_REPO_PATH__: string;
-declare const __APP_SPP_BLE_SERVICE__: string;
-declare const __APP_SPP_BLE_CHARACTERISTIC__: string;
