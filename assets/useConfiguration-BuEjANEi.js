7c6b5f904b1eeea81d20b6fbef825e11754c94f8
Vinay Bhupathi <vinay.bhupati@bridgethings.com>
2026-04-25 18:43:33 +0530

    updated web page

---
+++ assets/useConfiguration-BuEjANEi.js
@@ -0,0 +1,1 @@
+import{r as o,at as r}from"./index-BdjfIDfG.js";const e=()=>{const t=o.useContext(r);if(!t)throw new Error("useConfiguration must be used within a ConfigurationProvider");return t};export{e as u};
