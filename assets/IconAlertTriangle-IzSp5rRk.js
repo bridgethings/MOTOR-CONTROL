065e239c895ba53d065d592e4ab400cb71e36da2
Vinay Bhupathi <vinay.bhupati@bridgethings.com>
2026-04-29 11:04:59 +0530

    updated webpage

---
+++ assets/IconAlertTriangle-IzSp5rRk.js
@@ -0,0 +1,6 @@
+import{g as h,u,h as C,j as k,q as x,k as R,t as b,c as f}from"./index-D8TAX6dP.js";var t={root:"m_b183c0a2"};const j={},I=R((s,{color:e})=>({root:{"--code-bg":e?b(e,s):void 0}})),r=h((s,e)=>{const o=u("Code",j,s),{classNames:l,className:c,style:n,styles:d,unstyled:m,vars:p,color:M,block:a,variant:v,mod:i,...g}=o,y=C({name:"Code",props:o,classes:t,className:c,style:n,classNames:l,styles:d,unstyled:m,vars:p,varsResolver:I});return k.jsx(x,{component:a?"pre":"code",variant:v,ref:e,mod:[{block:a},i],...y("root"),...g,dir:"ltr"})});r.classes=t;r.displayName="@mantine/core/Code";/**
+ * @license @tabler/icons-react v3.29.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var T=f("outline","alert-triangle","IconAlertTriangle",[["path",{d:"M12 9v4",key:"svg-0"}],["path",{d:"M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z",key:"svg-1"}],["path",{d:"M12 16h.01",key:"svg-2"}]]);export{r as C,T as I};
